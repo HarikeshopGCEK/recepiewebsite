9eb9f5c2d427c608db7eb01f5503e2380379e617
Harikeshop <>
2025-07-19 21:35:32 +0530

    main

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Hello World</h1>
       <p className="text-lg">This is a simple landing page</p>
+      <p>Edit this landing page in the <span className="font-bold">src/app/page.js</span> file</p>
       <button className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 scale-105 hover:scale-110 transition-all duration-300">Click me</button>
     </div>
 
